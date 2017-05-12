1c66606b6f45b2c1c1b8ae2119f1eb87901d64ae
Rob <robert.uni177@hotmail.co.uk>
2017-05-12 12:44:03 +0100

    Tidying up 2

--- Trading/testingapp/testingapp/src/main/java/TestsOne.java
+++ Trading/testingapp/testingapp/src/main/java/TestsOne.java
@@ -5,13 +5,10 @@ import org.junit.Test;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
-/**
- * Created by rtaylor on 09/05/2017.
- */
 public class TestsOne {
 
-    public static String driverPath = "C:/Users/rtaylor/Downloads/chromedriver_win32/";
-    public static WebDriver driver;
+    private static String driverPath = "C:/Users/rtaylor/Downloads/chromedriver_win32/";
+    private static WebDriver driver;
 
     @BeforeClass
     public static void test() {
@@ -24,7 +21,7 @@ public class TestsOne {
     public void CreateEvent() {
         GoToGoogle();
         String expected = "https://www.google.co.uk/?gws_rd=ssl";
-        String actual = driver.getCurrentUrl().toString();
+        String actual = (driver.getCurrentUrl().toString());
         Assert.assertEquals(expected, actual);
     }
 
@@ -40,7 +37,7 @@ public class TestsOne {
         }
     }
 
-    public void GoToGoogle(){
+    private void GoToGoogle(){
         driver.navigate().to("http://www.google.co.uk");
     }
 
